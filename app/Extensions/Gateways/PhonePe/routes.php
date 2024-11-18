2fa391c1cb5ec7bac3f34b9fe7ca2802532a1141
Debargha Chattaraj <somachattaraj77@gmail.com>
2024-11-19 04:40:20 +0530

    Removed Redirect Route
    - and added an unnecessary route

--- app/Extensions/Gateways/PhonePe/routes.php
+++ app/Extensions/Gateways/PhonePe/routes.php
@@ -1,9 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Http\Request;
-
-Route::post('/phonepe/redirect', [\App\Extensions\Gateways\PhonePe\PhonePe::class, 'redirect'])->name('phonepe.redirect');
 
 Route::post('/phonepe/callback', [\App\Extensions\Gateways\PhonePe\PhonePe::class, 'callback'])->name('phonepe.callback');
+
+Route::get('/phonepe/debargha', function () {return redirect('https://debargha.in');});
